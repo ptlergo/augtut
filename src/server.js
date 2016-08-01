76e559d579fbc83bde3399122420825d0ee01125
ptlergo <ptlergo@gmail.com>
2016-08-01 02:34:34 -0400

    lint file and ignore node modules

--- src/server.js
+++ src/server.js
@@ -4,9 +4,10 @@ const app = express();
 app.post('/api/message', (req, res){
   console.log(req.body);
   res.status(200);
-})
+
+});
 
 const server = app.listen(5000, (){
   console.log('listening on port: 5000', server.address().port);
 
-})
+});
