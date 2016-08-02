5ffb531096cbe72e4a067df997444e2a6e6ee79c
ptlergo <ptlergo@gmail.com>
2016-08-02 10:49:18 -0400

    auth route

--- client/src/app/index.route.js
+++ client/src/app/index.route.js
@@ -1,12 +1,18 @@
 export function routerConfig ($stateProvider, $urlRouterProvider) {
   'ngInject';
   $stateProvider
-    .state('home', {
-      url: '/',
-      templateUrl: 'app/main/main.html',
-      controller: 'MainController',
-      controllerAs: 'main'
-    });
+  .state('home', {
+    url: '/',
+    templateUrl: 'app/main/main.html',
+    controller: 'MainController',
+    controllerAs: 'main'
+  })
+  .state('auth', {
+    url: '/auth',
+    templateUrl: 'app/main/main.html',
+    controller: 'MainController',
+    controllerAs: 'main'
+  });
 
   $urlRouterProvider.otherwise('/');
 }
