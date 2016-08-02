e105f586df14511668942cbed7ecdfcfd015269a
ptlergo <ptlergo@gmail.com>
2016-08-02 01:39:33 -0400

    getmessage function in angular created

--- client/src/app/main/main.controller.js
+++ client/src/app/main/main.controller.js
@@ -2,12 +2,21 @@ export class MainController {
   constructor ($http) {
     'ngInject';
 
-     this.$http = $http;
+    this.$http = $http;
+    this.getMessages();
+
+  }
+
+  //display message
+  getMessages() {
+    this.$http.get('http://localhost:5000/api/message').then((result) => {
+      console.log(result);
+    });
+
 
   }
 
   postMessage() {
-
     //implemment CORS to send access to NODE server
     this.$http.post('http://localhost:5000/api/message',{msg: this.message});
   }
