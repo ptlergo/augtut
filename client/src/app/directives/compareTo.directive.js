ff23de3239d028f477f1a5babb5314f10fec44aa
ptlergo <ptlergo@gmail.com>
2016-08-02 11:20:25 -0400

    model in compareto directive created

--- client/src/app/directives/compareTo.directive.js
+++ client/src/app/directives/compareTo.directive.js
@@ -2,9 +2,14 @@ export function CompareToDirective($parse) {
   'ngInject'
   return {
     require: 'ngModel',
-    link: function(scope, elm, attrs, ngModel){
+    link: (scope, elm, attrs, ngModel) => {
       const mainModel = $parse(attrs.compareTo);
       const secondModel = $parse(attrs.ngModel);
+
+      //watch for changes to models then compare validity
+      scope.$watch(attrs.ngModel, (newValue) => {
+        ngModel.$setValidity(attrs.name, newValue === mainModel(scope));
+      })
     }
 
   }
